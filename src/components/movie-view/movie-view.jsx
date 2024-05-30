3f12c3fef3a7c3bb69c28aebc04dd3352a40af57
raschkatie <raschkatie@gmail.com>
2024-05-30 16:36:58 -0400

    Updated styling

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,25 +2,25 @@ import './movie-view.scss';
 
 export const MovieView = ({ movie, onBackClick }) => {
     return (
-        <div>
+        <div className="movie-view-body">
 
             <div>
-                <img src={movie.image} alt={movie.title} />
+                <img className="movie-image" src={movie.image} alt={movie.title} />
             </div>
             <div>
-                <span>Title: </span>
+                <span className="bold-title">Title: </span>
                 <span>{movie.title}</span>
             </div>
             <div>
-                <span>Description: </span>
+                <span className="bold-title">Description: </span>
                 <span>{movie.description}</span>
             </div>
             <div>
-                <span>Director: </span>
+                <span className="bold-title">Director: </span>
                 <span>{movie.director.Name}</span>
             </div>
             <div>
-                <span>Genre: </span>
+                <span className="bold-title">Genre: </span>
                 <span>{movie.genre.Name}</span>
             </div>
 
