18ea8db3b4df3b7d2c82bb57b3ed653acccd8bc1
raschkatie <raschkatie@gmail.com>
2024-06-05 16:30:52 -0400

    Updated add/remove favorites methods

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -5,28 +5,25 @@ import { MovieCard } from "../movie-card/movie-card";
 export const FavoriteMovies = ({ movies, user }) => {
     let favoriteMovies = movies.filter(m => user.FavoriteMovies.includes(m._id));
     
-    if (!movies || !user.FavoriteMovies) {
-        return (
-            <div>
-                Uh oh! Looks like you don't have any favorite movies yet.
-            </div>
-        )
-    }
-
-    if (!Array.isArray(movies)) {
-        return;
-    }
-
     return (
-        <div>
-            <h2>Favorite Movies</h2>
+        <Row>
+            <Col md={12}>
+                <h2>Favorite Movies</h2>
+            </Col>
             <Row>
-                {favoriteMovies.map((movie) => (
-                    <Col key={movie.id} md={4}>
-                            <MovieCard movie={movie} />
-                    </Col>
-                ))}
+                {favoriteMovies.map((movie) => {
+                    return (
+                        <Col md={4}>
+                            <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+                                <MovieCard
+                                    movie={movie}
+                                    isFavorite={user.favoriteMovies}
+                                />
+                            </Link>
+                        </Col>
+                    );
+                })}
             </Row>
-        </div>
-    );
+        </Row>
+    )
 };
