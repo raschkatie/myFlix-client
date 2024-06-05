8645c6659295cff9d011201f78f73ccef5222b8b
raschkatie <raschkatie@gmail.com>
2024-06-05 12:11:52 -0400

    Corrected URL path

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -26,7 +26,7 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                                 <Nav.Link as={Link} to="/">
                                     Home
                                 </Nav.Link>
-                                <Nav.Link as={Link} to="/users/:userId">
+                                <Nav.Link as={Link} to="/profile">
                                     Profile
                                 </Nav.Link>
                                 <Nav.Link onClick={onLoggedOut}>
