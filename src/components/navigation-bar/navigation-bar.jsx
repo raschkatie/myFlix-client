e89840049c84bf4578d90271d2b20c269b7d0bfd
raschkatie <raschkatie@gmail.com>
2024-06-06 12:08:24 -0400

    Adjusted class names

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,15 +1,17 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+import "./navigation-bar.scss";
+
 export const NavigationBar = ({ user, onLoggedOut }) => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar expand="lg" >
             <Container>
                 <Navbar.Brand as={Link} to="/">
                     MyFlix
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Collapse className="nav-bar" id="basic-navbar-nav">
                     <Nav className="me-auto">
                         {!user && (
                             <>
