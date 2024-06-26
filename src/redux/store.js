2067de99a1f1fc6c4b4d7e632cfea3e5de76449a
raschkatie <raschkatie@gmail.com>
2024-06-26 16:48:50 -0400

    Corrected file path

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import moviesReducer from "./reducers/movies";
-import userReducer from "./reducers/user";
+import userReducer from "./reducers/user/user";
 
 export const store = configureStore({
     reducer: { movies: moviesReducer, user: userReducer }
